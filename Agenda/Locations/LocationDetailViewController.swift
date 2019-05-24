32ea199fdc8b70e093b0a4c25c3190b9b7ff60dc
ankykush <anky.kush@gmail.com>
2019-05-24 00:45:55 -0500

    added detail controllers

---
+++ Agenda/Locations/LocationDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailViewController.swift
+//  Agenda
+//
+//  Created by Ankush jain on 24/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
