246062614fd04a9f411584b4b62e93a6d9bee15e
ankykush <anky.kush@gmail.com>
2019-05-23 23:33:37 -0500

    Initial project

---
+++ Agenda/Locations/LocationsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationsViewController.swift
+//  Agenda
+//
+//  Created by Ankush jain on 23/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class LocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Agenda/Utils/AlertDisplayer.swift
@@ -0,0 +1,29 @@
+//
+//  AlertDisplayer.swift
+//  Agenda
+//
+//  Created by Ankush jain on 23/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol AlertDisplayer {
+    func displayAlert(with title: String, message: String, actions: [UIAlertAction]?)
+}
+
+extension AlertDisplayer where Self: UIViewController {
+    func displayAlert(with title: String, message: String, actions: [UIAlertAction]? = nil) {
+        guard presentedViewController == nil else {
+            return
+        }
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions?.forEach { action in
+            alertController.addAction(action)
+        }
+        present(alertController, animated: true)
+    }
+}
+
