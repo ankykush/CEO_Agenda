3eb07b3ae017c123d13a6830afdadd0fb494bdaf
Sravanthi Kuppireddy <sravanthi@Sravanthis-MacBook-Air.local>
2019-05-27 10:07:01 -0500

    Added tableview cell

---
+++ Agenda/Executive/Executive.swift
@@ -0,0 +1,9 @@
+//
+//  Executive.swift
+//  Agenda
+//
+//  Created by Sravanthi Kuppireddy on 5/24/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import Foundation
---
+++ Agenda/Executive/ExecutiveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExecutiveTableViewCell.swift
+//  Agenda
+//
+//  Created by Sravanthi Kuppireddy on 5/27/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class ExecutiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
