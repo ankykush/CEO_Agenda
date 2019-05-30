e53c649233611b69673213a302dca061a43d26b5
ankykush <anky.kush@gmail.com>
2019-05-29 22:31:46 -0500

    added contacts form

--- Agenda/Contacts/Controllers/ContactsViewController.swift
+++ Agenda/Contacts/Controllers/ContactsViewController.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Helper Methods
+    private func configureUI() {
+        hideNavigationBar()
+        tableView.tableFooterView = UIView()
     }
-    */
+    
+}
 
+//MARK: UITableViewDataSource Methods
+extension ContactsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell", for: indexPath) as? ContactTableViewCell else {
+            return UITableViewCell()
+        }
+        //cell.populate()
+        return cell
+    }
 }
---
+++ Agenda/Contacts/Views/ContactTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ContactTableViewCell.swift
+//  Agenda
+//
+//  Created by Ankush jain on 29/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var phoneNumberLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
