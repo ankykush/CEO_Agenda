7a1a8e2bd11cebeb7f8e5d6c6d0a2861448b1d6c
ankykush <anky.kush@gmail.com>
2019-05-29 22:14:38 -0500

    added schedule form

--- Agenda/Schedule/Controllers/ScheduleViewController.swift
+++ Agenda/Schedule/Controllers/ScheduleViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class ScheduleViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
 
+    //MARK: Helper Methods
     private func configureUI() {
         hideNavigationBar()
+        tableView.tableFooterView = UIView()
+        tableView.estimatedRowHeight = 120
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+//MARK: UITableViewDataSource Methods
+extension ScheduleViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ScheduleTableViewCell", for: indexPath) as? ScheduleTableViewCell else {
+            return UITableViewCell()
+        }
+        //cell.populate()
+        return cell
+    }
+}
+
+extension ScheduleViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
---
+++ Agenda/Schedule/Views/ScheduleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleTableViewCell.swift
+//  Agenda
+//
+//  Created by Ankush jain on 29/05/19.
+//  Copyright © 2019 TCS. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet weak var venueLbl: UILabel!
+    @IBOutlet weak var purposeLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func populate() {
+        
+    }
+
+}
